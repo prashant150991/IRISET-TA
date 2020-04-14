f6bdfe5b8a539b7c7634006487ba89a40512018d
itsmepsk <itsmepsk@gmail.com>
2020-04-14 23:41:40 +0530

    Created index file.

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--    <link href="./files/css/form.css" rel="stylesheet" type="text/css">-->
+    <script href="./files/js/jquery-3.4.1.min.js"></script>
+    <script href="./files/js/bootstrap.min.js"></script>
+    <link href="files/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div class="container align-items-center">
+
+    <div class="row align-content-center">
+        <div class="col align-content-center">
+            <a href = "./form.php">Click here to get started.</a>
+        </div>
+    </div>
+
+</div>
+
+</body>
