69e7a6e56ea6470d93a45b52bdc748e8ad3881c5
Prathamesh Kakade <itsmepsk@gmail.com>
2021-02-18 00:09:22 +0530

    Removed HTML errors from form.php

--- requires/form.php
+++ requires/form.php
@@ -1,15 +1,17 @@
-<html>
+
+<!doctype html>
+<html lang="en">
 
 <head>
     <title>Travel Allowance</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script href="../files/js/jquery-3.4.1.min.js"></script>
-    <script href="../files/js/bootstrap.min.js"></script>
+    <script src="../files/js/jquery-3.4.1.min.js"></script>
+    <script src="../files/js/bootstrap.min.js"></script>
     <link href="../files/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="../files/css/form.css" rel="stylesheet" type="text/css">
 
-    <script language="JavaScript">
+    <script>
         var count = 0;
         var count2 = 0;
 
@@ -549,38 +551,38 @@
             <div class='form-row'>
                 <div class='form-group col-sm-4 col-md-2 formLabel'>
                     <div class='fff'>
-                        <label for="inputName">Name *</label>
-                        <input type='text' name = 'name' required = 'required' placeholder='Your Name' value = '' class="form-control form-control-lg formData" id="inputEmail4">
+                        <label for="name">Name *</label>
+                        <input type='text' name = 'name' required = 'required' placeholder='Your Name' value = '' class="form-control form-control-lg formData" id="name">
                     </div>
                 </div>
                 <div class='form-group col-sm-4 col-md-2 formLabel'>
                     <div class='fff'>
-                        <label for="inputDesignation">Designation *</label>
-                        <input type='text' name = 'designation' required = 'required' value='' placeholder='Designation'  class="form-control form-control-lg formData" id="inputEmail4">
+                        <label for="designation">Designation *</label>
+                        <input type='text' name = 'designation' required = 'required' value='' placeholder='Designation'  class="form-control form-control-lg formData" id="designation">
                     </div>
                 </div>
                 <div class='form-group col-sm-4 col-md-2 formLabel'>
                     <div class='fff'>
-                        <label for='inputMonth'>Month *</label>
-                        <input type='text' name='month' required = 'required' placeholder='TA Month' value = '' class="form-control form-control-lg formData" id="inputEmail4">
+                        <label for='month'>Month *</label>
+                        <input type='text' name='month' required = 'required' placeholder='TA Month' value = '' class="form-control form-control-lg formData" id="month">
                     </div>
                 </div>
                 <div class='form-group col-sm-4 col-md-2 formLabel'>
                     <div class='fff'>
-                        <label for='inputPayBand'>Grade Pay *</label>
-                        <input type='text' name = 'gradePay' value='' placeholder='Grade Pay' required = 'required' class="form-control form-control-lg formData" id="inputEmail4">
+                        <label for='payBand'>Grade Pay *</label>
+                        <input type='text' name = 'gradePay' value='' placeholder='Grade Pay' required = 'required' class="form-control form-control-lg formData" id="gradePay">
                     </div>
                 </div>
                 <div class='form-group col-sm-4 col-md-2 formLabel'>
                     <div class='fff'>
-                        <label for='inputBasicPay'>Basic Pay </label>
-                        <input type='text' name='basicPay' value='' placeholder='Your Basic Pay'  class="form-control form-control-lg formData" id="inputEmail4">
+                        <label for='gradePay'>Basic Pay </label>
+                        <input type='text' name='basicPay' value='' placeholder='Your Basic Pay'  class="form-control form-control-lg formData" id="basicPay">
                     </div>
                 </div>
                 <div class='form-group col-sm-4 col-md-2 formLabel'>
                     <div class='fff'>
-                        <label for='inputDateOfJoining'>Date of Joining </label>
-                        <input type='text' name='dateOfJoining' value='' placeholder='Date Of Joining'  class="form-control form-control-lg formData" id="inputEmail4">
+                        <label for='dateOfJoining'>Date of Joining </label>
+                        <input type='text' name='dateOfJoining' value='' placeholder='Date Of Joining'  class="form-control form-control-lg formData" id="dateOfJoining">
                     </div>
                 </div>
             </div>
@@ -590,7 +592,7 @@
                 <div class='form-group col-sm-12 col-md-12 formLabel'>
                     <div class='fff'>
                         <label for='zoneOrUnit'>Zone/Unit *</label>
-                        <select  name = "zoneOrUnit" class="form-control form-control-lg" id="exampleFormControlSelect1" style="text-align: center">
+                        <select  name = "zoneOrUnit" id ="zoneOrUnit" class="form-control form-control-lg" id="exampleFormControlSelect1" style="text-align: center">
                             <option>IRISET</option>
                             <option>ECoR</option>
                             <option>NR</option>
@@ -602,20 +604,20 @@
             <div class="form-row" style="width:100%">
                 <div class='form-group col-sm-12 col-md-4 formLabel'>
                     <div class='fff'>
-                        <label for='inputDateOfJoining'>Division </label>
-                        <input type='text' name='division' value='' placeholder='Division'  class="form-control form-control-lg formData" id="inputEmail4">
+                        <label for='division'>Division </label>
+                        <input type='text' name='division' value='' placeholder='Division'  class="form-control form-control-lg formData" id="division">
                     </div>
                 </div>
                 <div class='form-group col-sm-12 col-md-4 formLabel'>
                     <div class='fff'>
-                        <label for='inputDateOfJoining'>PF Number </label>
-                        <input type='text' name='pfNumber' value='' placeholder='PF NUmber'  class="form-control form-control-lg formData" id="inputEmail4">
+                        <label for='pfNumber'>PF Number </label>
+                        <input type='text' name='pfNumber' value='' placeholder='PF NUmber'  class="form-control form-control-lg formData" id="pfNumber">
                     </div>
                 </div>
                 <div class='form-group col-sm-12 col-md-4 formLabel'>
                     <div class='fff'>
-                        <label for='inputDateOfJoining'>Per Day TA in Rupees*</label>
-                        <input type='number' name='perDayTA' value=900 placeholder='Per Day TA' required = 'required' class="form-control form-control-lg formData" id="inputEmail4">
+                        <label for='perDayTA'>Per Day TA in Rupees*</label>
+                        <input type='number' name='perDayTA' value=900 placeholder='Per Day TA' required = 'required' class="form-control form-control-lg formData" id="perDayTA">
                     </div>
                 </div>
             </div>
