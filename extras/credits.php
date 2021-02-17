f5e2b8f30f161cbce69a26970703afbc44793788
Prathamesh Kakade <itsmepsk@gmail.com>
2021-02-18 00:16:04 +0530

    Removed HTML errors from credits.php

--- extras/credits.php
+++ extras/credits.php
@@ -17,8 +17,8 @@ class Credits {
         echo "<div class='col-md-6 offset-md-3 uuu'>";
         echo "<div class='list-group'>";
         echo "<a href='../'  class='list-group-item list-group-item-primary' style='text-align: center'><h3>Credits</h3></a>";
-        echo " <a target='_blank' href='https://github.com/itsmepsk' class='list-group-item list-group-item' style='text-align: center'>Prathamesh Kakade (ADSTE Pakala)</a>";
-        echo "<li class='list-group-item' style='text-align: center'>Karamveer Prasad</li>";
+        echo "<a target='_blank' href='https://github.com/itsmepsk' class='list-group-item list-group-item' style='text-align: center'>Prathamesh Kakade (ADSTE Pakala)</a>";
+        echo "<a href='#' class='list-group-item' style='text-align: center'>Karamveer Prasad</li>";
         echo "</div>";
         echo "</div>";
         echo "</div>";
@@ -27,15 +27,15 @@ class Credits {
     }
 
     public function displayHead() {
-
-        echo "<html>";
+        echo "<!DOCTYPE html>";
+        echo "<html lang='en'>";
         echo "<head>";
         echo "<title>Travel Allowance Generator</title>";
         echo "<meta charset='utf-8'>";
         echo "<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>";
         echo "<link href='../files/css/index.css' rel='stylesheet' type='text/css'>";
-        echo "<script href='../files/js/jquery-3.4.1.min.js'></script>";
-        echo "<script href='../files/js/bootstrap.min.js'></script>";
+        echo "<script src='../files/js/jquery-3.4.1.min.js'></script>";
+        echo "<script src='../files/js/bootstrap.min.js'></script>";
         echo "<link href='../files/css/bootstrap.min.css' rel='stylesheet' type='text/css'>";
         echo "</head>";
 
